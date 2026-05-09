ac2b93af8772360b5312285665e33faf0cc8eb64
lagyska <lot472784@gmail.com>
2026-05-09 11:33:59 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -28,11 +28,24 @@ class Player(GameSprite):
             self.rect.y += self.speed
 
 class Ball(GameSprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__(player_image, player_x, player_y, size_x, size_y, player_speed)
+        self.speed_y = player_speed
+        self.speed_x = player_speed
+        self.coef_x = 1
+        self.coef_y = 1
     def update(self):
-        self.rect.y += self.speed
-        if self.rect.y >= win_h:
-            self.rect.y = randint(-65, 0)
-            self.rect.x = randint(20, win_w - 20)
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        def switch_x(self):
+            self.speed_x *= -1
+            self.coef_x += 0.05
+            self.speed_x *= coef_x
+            
+        def switch_y(self):
+            self.speed_y *= -1  
+            self.coef_y += 0.05
+            self.speed_y *= coef_x
 
 win_w = 700
 win_h = 500
@@ -48,13 +61,18 @@ background = transform.scale(
 
 player_left = Player("i.webp", 50, 10, 48, 130, 10)
 player_right = Player("rac.png", win_w - 30 - 48, win_h - 10 - 130, 48, 130, 10)
+ball = GameSprite("bal.jpg", win_w / 2 +20, win_h / 2 - 20, 50, 50, 17)
+
+font.init()
+font_text = font.Font(None, 72)
+win_right_text = font_text.render("WIN RIGHT!",1,(0,255,0))
+win_left_text = font_text.render("WIN FEFT!",1,(0,255,0))
+result_text = win_left_text
 
 clock = time.Clock()
 FPS = 60
 run = True
 finish = False
-
-ball = GameSprite("bal.jpg", win_w / 2, win_h / 2, 50, 50, 0)
 
 while run:
     for e in event.get():
@@ -64,11 +82,27 @@ while run:
     if not finish:
         player_left.update_l()
         player_right.update_r()
+        ball.update()
 
+        if ball.rect.y < 0 or ball.rect.y > win_h - ball.image.get_height():
+            ball.switch_y()
+
+        if sprite.collide_rect(ball, player_left) or sprite.collide_rect(ball, player_right):
+            ball.switch_y()
+
+        if ball.rect.x < 0:
+            result_text = win_right_text
+            finish = True
+
+        if ball.rect.x > win_w - ball.image.ret_width():
+            result_text = win_left_text
+            finis = True
         window.blit(background, (0, 0))
         player_left.reset()
         player_right.reset()
         ball.reset()
+        else:
+            windows.blit(result_text)
 
     display.update()
     clock.tick(FPS)
